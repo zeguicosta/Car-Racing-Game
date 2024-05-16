e60a050b3564bcdbebd1b3d7d5e0fb329ea65473
Guilherme <joseguilhermecosta@gmail.com>
2024-05-16 18:53:06 -0300

    Adicionando colisão com a pista.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ grass = scale_image(pygame.image.load('imgs/grass.jpg'), 2.5)
 track = scale_image(pygame.image.load('imgs/track.png'), 0.8)
 
 track_border = scale_image(pygame.image.load('imgs/track-border.png'), 0.8)
+track_border_mask = pygame.mask.from_surface(track_border) # Máscara de colisão do circuito
 finish = pygame.image.load('imgs/finish.png')
 
 red_car = scale_image(pygame.image.load('imgs/red-car.png'), 1.5)
@@ -21,7 +22,7 @@ green_car = scale_image(pygame.image.load('imgs/green-car.png'), 0.55)
 # Criando a janela do jogo
 width, height = track.get_width(), track.get_height() # Pegando o tamanho do circuito
 window = pygame.display.set_mode((width, height)) # Tornando a janela do tamanho do cirtuito
-pygame.display.set_caption('FORMULA E SIMULATOR')
+pygame.display.set_caption('FORMULA E SIMULATOR') # Título do jogo
 
 
 # Classe pai dos carros
@@ -50,13 +51,14 @@ class AbstractCar:
         # então essa será a velocidade do carro. Caso contrário, a velocidade máxima
         # será a velocidade do carro.
         self.vel = min(self.vel + self.acceleration, self.max_vel)
-        self.move()
+        self.move() # Move o carro
 
+    # Função para dar ré
     def move_backward(self):
         # Diminui a velocidade pela a aceleração, até que a velocidade fique negativa
         # e seja igual a metade da velocidade máxima positiva.
         self.vel = max(self.vel - self.acceleration, -self.max_vel/2)
-        self.move()
+        self.move() # Move o carro com a velocidade negativa
 
     # Função para mover o carro
     def move(self):
@@ -68,6 +70,14 @@ class AbstractCar:
 
         self.y -= vertical # Move o carro verticalmente
         self.x -= horizontal # Move o carro horizontalmente
+
+    # Função para colisão
+    def collide(self, mask, x=0, y=0):
+        car_mask = pygame.mask.from_surface(self.img) # Máscara de colisão do carro
+        offset = (int(self.x - x), int(self.y - y))
+        # Ponto de interseção
+        poi = mask.overlap(car_mask, offset)
+        return poi
 
 
 # Classe filha para o carro do player
@@ -81,6 +91,11 @@ class PlayerCar(AbstractCar):
         self.vel = max(self.vel - self.acceleration / 1.5, 0)
         self.move() # Continua movendo o carro ao desacelerar
 
+    # Função para rebater o carro ao colidir
+    def bounce(self):
+        self.vel = -self.vel # Inverte a direção da velocidade
+        self.move() # Move o carro assim que inverte a velocidade
+
 
 # Função para renderizar as imagens
 def render(window, images, player_car):
@@ -90,8 +105,9 @@ def render(window, images, player_car):
     player_car.render(window)
     pygame.display.update() # Atualiza a tela
 
+# Função para mover o carro do player
 def move_player(player_car):
-    keys = pygame.key.get_pressed() # Se uma tecla for pressionada
+    keys = pygame.key.get_pressed()
     moved = False
 
     # Se a tecla A for pressionada o carro rotaciona para a esquerda
@@ -100,13 +116,16 @@ def move_player(player_car):
     # Se a tecla D for pressionada o carro rotaciona para a direita
     if keys[pygame.K_d]:
         player_car.rotate(right=True)
+    # Se a tecla W for pressionada o carro move para frente
     if keys[pygame.K_w]:
         moved = True
         player_car.move_forward()
+    # Se a tecla S for pressionada o carro move para trás
     if keys[pygame.K_s]:
         moved = True
         player_car.move_backward()
 
+    # Se o carro não estiver sendo movido pelo player, reduz a velocidade
     if not moved:
         player_car.reduce_speed()
 
@@ -117,7 +136,7 @@ clock = pygame.time.Clock()
 # Lista de imagens e suas posições de renderização
 images = [(grass, (0, 0)), (track, (0, 0))]
 # Carro do jogador com velocidade máxima 5 e velocidade de rotação 5
-player_car = PlayerCar(5, 4)
+player_car = PlayerCar(4, 5)
 
 
 # Game Loop
@@ -126,12 +145,16 @@ while run:
 
     render(window, images, player_car)
 
-    # Checa os eventos
+    # Verifica os eventos
     for event in pygame.event.get():
         if event.type == pygame.QUIT: # Encerra o Game Loop ao fechar a janela
             run = False
             break
 
     move_player(player_car)
+
+    # Verifica se o carro colidiu
+    if player_car.collide(track_border_mask):
+        player_car.bounce()
     
 pygame.quit()
